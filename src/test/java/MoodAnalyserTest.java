6309daabf0a18653456efc86167cf076594a86a6
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-09 21:31:06 +0530

    analyse mood method to return Happy to pass test

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,16 +1,24 @@
 import com.bridgelabz.moodanalyser.MoodAnalyser;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
 public class MoodAnalyserTest {
+    static MoodAnalyser moodAnalyser;
 
+    @BeforeAll
+    public static void init(){
+        moodAnalyser = new MoodAnalyser();
+    }
     @Test
     public void givenMessageWhenSadShouldReturnSad(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
         String result = moodAnalyser.analyseMood("I am in Sad Mood");
         Assertions.assertSame("SAD",result);
-
-
     }
+        @Test
+     public void givenMessageWhenHappyShouldReturnHappy(){
+        String result = moodAnalyser.analyseMood("I am in Happy Mood");
+        Assertions.assertSame("HAPPY",result);
+     }
 }
